119f3fc6aabe8872601e808c404ae001419dae21
Nadia <Nadia@gmail.com>
2022-11-12 19:28:55 +0100

    Page blog

--- moblog.php
+++ moblog.php
@@ -1,1 +1,68 @@
-page blog !!!!!!!
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MyOrphan</title>
+    <?php include('composant/header.html')?>
+</head>
+
+<body>
+<?php include ("composant/menu.html")?>
+
+<div class="container page-container">
+
+    <div class="don-text text-center mt-2 mb-5">
+        <br><br>
+        <h1>BLOG</h1>
+        <h4>-*-*-*-*-*-</h4>
+    </div>
+
+    <div class="container nos-projets-content">
+        <div class="row">
+            <div class="col text-center">
+                <div class="card text-center" style="width: 18rem;" >
+                    <img src="images/person-age.jpeg"class="card-img-top"  alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Titre blog 1</h5>
+                        <p class="card-text">
+                            Text blog 1
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+
+                <div class="card text-center" style="width: 18rem;" >
+                    <img src="images/parinnage.jpg"class="card-img-top"  alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Titre blog 2</h5>
+                        <p class="card-text">
+                            Text blog 2
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col">
+
+                <div class="card text-center" style="width: 18rem;" >
+                    <img src="images/orphan01.jpg"class="card-img-top"  alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Titre blog 3</h5>
+                        <p class="card-text">
+                            Text blog 3
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+
+
+</html>
